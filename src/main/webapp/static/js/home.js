49eb79d76b41fca8e49b303b2148232a49202a06
jasonsongzhong <1634744178@qq.com>
2022-03-25 17:33:53 +0800

    更改界面

--- src/main/webapp/static/js/home.js
+++ src/main/webapp/static/js/home.js
@@ -1,5 +1,5 @@
 $(function (){
-    var mySwiper = new Swiper ('.swiper', {
+    var mySwiper = new Swiper ('#banner', {
         direction: 'horizontal', // 垂直切换选项
         loop: true, // 循环模式选项
         speed: 1000, //图片切换速度
@@ -25,5 +25,43 @@ $(function (){
         scrollbar: {
             el: '.swiper-scrollbar',
         },
+    })
+
+    var gallerySwiper = new Swiper('#service_category', {
+        direction: 'vertical', // 垂直切换选项
+        //抓手光标
+        grabCursor : true,
+        spaceBetween: 10,
+        thumbs: {
+            swiper: {
+                //抓手光标
+                grabCursor : true,
+                //自动高度调整
+                autoHeight: true,
+
+                direction: 'vertical', // 垂直切换选项
+                el: '#thumbs',
+                spaceBetween: 5,
+                slidesPerView: 3,
+                watchSlidesVisibility: true,/*避免出现bug*/
+                scrollbar: {
+                    el: '.swiper-scrollbar',
+                },
+            },
+        }
+
+        // 左右翻页
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+        // autoplay: {
+        //     disableOnInteraction: false,
+        // },
+
+        // 分页器
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
     })
 })
